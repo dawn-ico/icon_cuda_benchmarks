a6858745b61e5bdcc926be761f14b0601253374b
Giacomo Serafini <giacomo.serafini@mail.polimi.it>
2020-04-24 09:34:32 +0200

    remove sbatch

--- run_daint.sh
+++ run_daint.sh
@@ -1,17 +1,6 @@
-#!/bin/bash -l
-#
-#SBATCH --job-name="diamond_stencil_test"
-#SBATCH --time=00:05:00
-#SBATCH --nodes=1
-#SBATCH --account c14
-#SBATCH --partition debug
-#SBATCH --gres=gpu:1
-#SBATCH --constraint=gpu
-#SBATCH --ntasks-per-node=1
-#SBATCH --output=diamond_stencil_test.%j.output.log
-#SBATCH --error=diamond_stencil_test.%j.error.log
+#!/bin/bash
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/project/c14/install/daint/atlas_install/release/cpu/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/project/c14/install/daint/eckit_install/lib
 
-srun -C gpu ./out 64
+srun --account c14 -C gpu --partition debug --gres=gpu:1 ./out 64
