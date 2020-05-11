8f19a082769bbd555351c50cce7399e134811577
Matthias Roethlin <Matthias.Roethlin@MeteoSwiss.ch>
2020-05-11 13:21:38 +0200

    new run and build scripts for daint

--- build_daint.sh
+++ build_daint.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 module load gcc/8.3.0
-module load cudatoolkit/10.1.105_3.27-7.0.1.1_4.1__ga311ce7 
+module load cudatoolkit/10.1.105_3.27-7.0.1.1_4.1__ga311ce7
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/project/c14/install/daint/atlas_install
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/project/c14/install/daint/eckit_install
+atlasDir=/scratch/snx3000/mroeth/daint/spack/spack-install/daint/atlas/develop/gcc/shastqtqvp3umrxnbbi24qo22dgg4iar/
+eckitDir=/scratch/snx3000/mroeth/daint/spack/spack-install/daint/eckit/master/gcc/smpn5fkts67iyfyseaspzhcnzfzvtlxk/
+atlasUtilsDir=/scratch/snx3000/mroeth/daint/spack/spack-install/daint/atlas_utilities/master/gcc/xfv37ptrvuf657srvivvjv472ijflrd3
 
-atlasInc=/project/c14/install/daint/atlas_install/release/cpu/include
-eckitInc=/project/c14/install/daint/eckit_install/include
+atlasInc=${atlasDir}/include
+eckitInc=${eckitDir}/include
 
-atlasLib=/project/c14/install/daint/atlas_install/release/cpu/lib
-eckitLib=/project/c14/install/daint/eckit_install/lib
+atlasLib=${atlasDir}/lib
+eckitLib=${eckitDir}/lib
 
-atlasUtilsInc=/scratch/snx3000/mroeth/atlas_utilities/utils
-atlasIOInc=/scratch/snx3000/mroeth/atlas_utilities/stencils
+atlasUtilsInc=${atlasUtilsDir}/include
+atlasUtilsLib=${atlasUtilsDir}/lib
 
 cudaInc=/opt/nvidia/cudatoolkit10/10.1.105_3.27-7.0.1.1_4.1__ga311ce7/include
 cudaLib=/opt/nvidia/cudatoolkit10/10.1.105_3.27-7.0.1.1_4.1__ga311ce7/lib64
@@ -21,8 +22,6 @@ cudaLib=/opt/nvidia/cudatoolkit10/10.1.105_3.27-7.0.1.1_4.1__ga311ce7/lib64
 #nvcc -c -o cuda_stencil.o cuda_stencil.cu -std=c++14 -arch=sm_60 -I$atlasInc -I$eckitInc -L$atlasLib -latlas -L$eckitLib -leckit -I$atlasUtilsInc
 nvcc -c -o diamond_stencil.o diamond_stencil.cu -std=c++14 -arch=sm_60 -I$atlasInc -I$eckitInc -L$atlasLib -latlas -L$eckitLib -leckit -I$atlasUtilsInc
 
-g++ -o out diamond_driver.cpp diamond_stencil.o $atlasIOInc/io/atlasIO.cpp $atlasUtilsInc/AtlasCartesianWrapper.cpp $atlasUtilsInc/AtlasExtractSubmesh.cpp $atlasUtilsInc/GenerateRectAtlasMesh.cpp -std=c++17 -I$atlasUtilsInc -I$atlasIOInc -I$atlasInc -I$eckitInc -L$atlasLib -latlas -L$eckitLib -leckit -I$cudaInc -L$cudaLib -lcudart_static -pthread -lrt -ldl
-#g++ -o out cuda_driver.cpp cuda_stencil.o $atlasUtilsInc/AtlasCartesianWrapper.cpp $atlasUtilsInc/AtlasExtractSubmesh.cpp $atlasUtilsInc/GenerateRectAtlasMesh.cpp -std=c++17 -I$atlasUtilsInc -I$atlasInc -I$eckitInc -L$atlasLib -latlas -L$eckitLib -leckit -I$cudaInc -L$cudaLib -lcudart_static -pthread -lrt -ldl
+echo "g++ -o out diamond_driver.cpp diamond_stencil.o  -std=c++17 -I$atlasUtilsInc -I$atlasInc -I$eckitInc -L$atlasUtilsLib -latlasIOLib  -latlasUtilsLib -L$atlasLib -latlas -L$eckitLib -leckit -I$cudaInc -L$cudaLib -lcudart_static -pthread -lrt -ldl"
 
-
-
+g++ -o out diamond_driver.cpp diamond_stencil.o  -std=c++17 -I$atlasUtilsInc -I$atlasInc -I$eckitInc -L$atlasUtilsLib -latlasIOLib  -latlasUtilsLib -L$atlasLib -latlas -L$eckitLib -leckit -I$cudaInc -L$cudaLib -lcudart_static -pthread -lrt -ldl
--- run_daint.sh
+++ run_daint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/project/c14/install/daint/atlas_install/release/cpu/lib
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/project/c14/install/daint/eckit_install/lib
+module load gcc/8.3.0
+module load cudatoolkit/10.1.105_3.27-7.0.1.1_4.1__ga311ce7
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/scratch/snx3000/mroeth/daint/spack/spack-install/daint/atlas/develop/gcc/shastqtqvp3umrxnbbi24qo22dgg4iar/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/scratch/snx3000/mroeth/daint/spack/spack-install/daint/eckit/master/gcc/smpn5fkts67iyfyseaspzhcnzfzvtlxk/lib
 
 srun --account c14 -C gpu --partition debug --gres=gpu:1 -u ./out $1
